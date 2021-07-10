0ee84302fb2aca5b668e394a2aefdbcca018890f
Sumit Garg <garg47856@gmail.com>
2021-07-10 20:36:11 +0530

    My first project Google-Clone

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Home from "./pages/Home";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyDmq7tHxscY9AsznXCfWOPRJ9iBbpsd54M";
+
+export default API_KEY;
